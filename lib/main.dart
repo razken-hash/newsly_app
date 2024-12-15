1053d297f127e434d2f0354730b7205649425b45
razken-hash <ka_kenniche@esi.dz>
2024-12-15 12:24:46 +0100

    Integrating Firebase Successfully + Some Changes In build.gradle File

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:newsly_app/core/screens/news_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyDTviih_hIpzCZkh291X1HiTson_bbOapU",
+      appId: "1:776324892066:android:398efb430b9ff61ea7defb",
+      messagingSenderId: "",
+      projectId: "newsly-app-c4e30",
+    ),
+  );
   runApp(
     const NewslyApp(),
   );
